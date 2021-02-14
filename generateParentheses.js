a4f1f86184b33fc1a6b10b0777c91c1dad9d3a20
Chondan Susuwan <chondansusuwan@outlook.coom>
2021-02-14 11:22:29 +0700

    added generateParentheses

--- generateParentheses.js
+++ generateParentheses.js
@@ -1,13 +1,44 @@
-const generateParentheses = n => {
+const getPermutations = (prefix, arr, memo={}) => {
 
-	// n = 3 -> there are 6 position in each possible output e.g. '((()))'
-	// so we need to define the position of the opening parentheses -> there are nCr possile ways to generate
-	// after generating, we need to check wheter the output is valid
-	// ( ( ( ) ) ) -> should be only one opening parentheses after position n
+	const key = prefix.join('') + '_' + arr.join('');
+	if (key in memo) return null;
+	if (arr.length == 1) return [prefix.concat(arr)];
 
-	
-
+	const ans = [];
+	for (let i = 0; i < arr.length; i++) {
+		const remain = arr.slice(0, i).concat(arr.slice(i + 1));
+		const result = getPermutations(prefix.concat(arr[i]), remain, memo);
+		if (result != null) ans.push(...result);
+	}
+	memo[key] = ans;
+	return ans;
 
 }
 
+const checkCloseProperly = str => {
+
+	if (str[0] == ')') return false;
+
+	const stack = [];
+	for (char of str) {
+		if (char == '(') stack.push('(');
+		else stack.pop();
+	}
+	return stack.length == 0 ? true : false;
+
+}
+
+const generateParentheses = n => {
+
+	const parenthesesArray = ("(".repeat(n) + ")".repeat(n)).split('');
+	const allParentheses = [...new Set(getPermutations([], parenthesesArray).map(p => p.join('')))];
+	return allParentheses.filter(p => checkCloseProperly(p));
+
+}
+
+console.log(generateParentheses(1));
+console.log(generateParentheses(2));
 console.log(generateParentheses(3));
+console.log(generateParentheses(4));
+console.log(generateParentheses(5));
+console.log(generateParentheses(6));
